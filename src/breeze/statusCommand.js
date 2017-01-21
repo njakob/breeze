a67b19a1aa2b62aa419b4e317d48782d4586382b
Nicolas Jakob <nicow.jakob@gmail.com>
2017-01-21 12:11:14 +0100

    More core functions into a folder

--- src/breeze/statusCommand.js
+++ src/breeze/statusCommand.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import status from '../status';
+import status from 'core/status';
 import type { Context } from './common';
 
 export default async function statusCommand(ctx: Context): Promise<*> {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 /* @flow */
 
-export { default as status } from './status';
+export { default as status } from 'core/status';
