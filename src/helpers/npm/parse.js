ae6450ef7f9ef21af3fc372cdb9ad756764dcdc5
Nicolas Jakob <nicow.jakob@gmail.com>
2017-02-19 19:23:03 +0100

    Refactor NPM parse helper to use new fs helper

--- src/helpers/npm/parse.js
+++ src/helpers/npm/parse.js
@@ -1,43 +1,45 @@
 /* @flow */
 
-import fs from 'fs';
 import path from 'path';
-import type { Bugsy } from 'bugsy';
 import { parseParcel } from '@njakob/parcel';
+import * as fsHelpers from 'helpers/fs';
 import * as errorHelpers from 'helpers/error';
 
 export type ParseResult = {
   version: string;
 };
 
+async function readPackage(directory: string): Promise<string> {
+  try {
+    return await fsHelpers.readFile({
+      filePath: path.join(directory, 'package.json')
+    }).toString('utf8');
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw errorHelpers.npmPackageNotFound();
+    } else {
+      throw errorHelpers.convert(err);
+    }
+  }
+}
+
+function parsePackage(data: string): any {
+  try {
+    return JSON.parse(data);
+  } catch (innerErr) {
+    throw errorHelpers.npmPackageUnparseable();
+  }
+}
+
 export default async function parse(directory: string): Promise<ParseResult> {
-  return new Promise((resolve: (result: ParseResult) => void, reject: (err: Bugsy) => void) => {
-    const packageFilePath = path.join(directory, 'package.json');
+  const data = await readPackage(directory);
+  const { version } = parseParcel(parsePackage(data));
 
-    fs.readFile(packageFilePath, 'utf8', (err: ?ErrnoError, data: string): void => {
-      if (err) {
-        if (err.code === 'ENOENT') {
-          return reject(errorHelpers.npmPackageNotFound());
-        }
-        return reject(errorHelpers.convert(err));
-      }
+  if (!version) {
+    throw errorHelpers.npmPackageUnparseable();
+  }
 
-      let pkg;
-      try {
-        pkg = JSON.parse(data);
-      } catch (innerErr) {
-        return reject(errorHelpers.npmPackageUnparseable());
-      }
-
-      const { version } = parseParcel(pkg);
-
-      if (!version) {
-        return reject(errorHelpers.npmPackageUnparseable());
-      }
-
-      return resolve({
-        version,
-      });
-    });
-  });
+  return {
+    version,
+  };
 }
